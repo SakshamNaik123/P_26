fad229f44e79ae1010ed2a2e51ab1895a97d0409
SakshamNaik123 <78081850+SakshamNaik123@users.noreply.github.com>
2021-02-23 13:05:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var  background , chickenBiryani;
+var backgroundImg, chickenBiryaniImg;
+function preload(){
+
+
+}
+
+function setup() {
+  createCanvas(1800,3800);
+} 
+
+function draw() {
+  
+  background("blue");  
+  
+
+  drawSprites();
+
+  
+
+  textSize(30);
+  fill("yellow");
+  text("R E C I P E",100,50);
+  text("=> To make this delightful biryani, soak saffron in water to prepare saffron water.",100,100);
+  text("=> take a large bowl mix chicken with yoghurt (curd), fried onion, red chilli powder, salt, coriander leaves",100,200);
+  text("=> mint leaves, ginger and garlic paste. Mix all the ingredients well and let the chicken marinate for about 2 hours",100,300);
+  text("=> When the chicken is marinated, add garam masala in it",100,400); 
+  text("=> Once the chicken is marinated, melt ghee in a handi over medium flame",100,500);
+  text(" => When ghee is melted, add marinated chicken in it and fry for 2-3 minutes",100,600);
+  text(" => Then, add water in it and cook the chicken on low-medium flame",100,700);
+  text(" => Put the lid on vessel once the chicken is cooked.",100,800);
+  text("=> Now, add 70 per cent cooked basmati rice in it",100,900);
+  text(" => Above the rice layer, add brown or fried onion, ghee and saffron water",100,1000);
+  text("=> (saffron soaked in 2 tbsp water) and a little water",100,1100); 
+  text("=>  Knead a quick dough with wheat flour and seal the handi with this dough,",100,1200);
+  text("=> and then cover with a lid. Seal the handi and let the biryani stay like this for 20 minutes",100,1300);
+  text(" => Serve it hot with raita",100,1400);
+  
+  text("I N G R E D I E N T S",100,1500);
+  text("350 gm chopped chicken",100,1600);
+  text("20 gm garam masala powder",100,1700);
+  text("10 ml water",100,1800);
+  text("300 gm boiled basmati rice",100,1900);
+  text("30 ml ghee",100,2000);
+  text("1 cup wheat flour",100,2100);
+  text("For Marination",100,2200);
+  text("60 gm beaten yoghurt (curd)",100,2300);
+  text("15 gm ginger paste",100,2400);
+  text("1 gm mint leaves",100,2500);
+  text("1 tablespoon coriander leaves",100,2600);
+  text("15 gm garlic paste",100,2700);
+  text("salt as required",100,2800);
+  text("10 gm thinly sliced onion",100,2900);
+  text("7 gm red chilli powder",100,3000);
+  text("For Garnishing",100,3100);
+  text("2 dashes saffron",100,3200);
+  text("5 ml ghee",100,3300);
+  text("2 tablespoon onion",100,3400);
+  text("3 tablespoon water",100,3500);
+  
+   
+}
